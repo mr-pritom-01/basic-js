6e7ccc0e1308bc8a5c49c7c1f785c0f7793d0c76
Pritom Das <pritom6221@gmail.com>
2023-08-08 21:08:44 +0600

    edited some code

---
+++ ternary.js
@@ -0,0 +1,12 @@
+var isRaining = false;
+
+// if(isRaining == true){
+//     console.log('baire jabona');
+// }
+// else{
+//     console.log('baire jabo');
+// }
+
+isRaining == true ? console.log('baire jabo na') : console.log('baire jabo')
+
+//nested condition missing have to parctice and watch
